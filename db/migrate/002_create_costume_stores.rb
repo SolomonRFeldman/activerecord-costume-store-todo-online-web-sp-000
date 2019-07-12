f0a9111120d77e9b03c31f3d5073f288a291c872
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-12 13:48:21 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,8 +7,9 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.timestamp :opening_time
-      t.timestamp :closing_time
+      t.datetime :opening_time
+      t.datetime :closing_time
+      t.timestamps
     end
   end
   
